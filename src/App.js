f5e7594c237575567fbdff5e6a64435c9c6972ce
DLGiaKiet <giakiet20012004@gmail.com>
2024-11-22 20:15:20 +0700

    Color Box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ColorBox from './components/ColorBox';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1>
         Welcome
       </h1>
+      <ColorBox />
     </div>
   );
 }
